be9adb7ca3f5575c45e3743c2219efee7c01ca92
NF Devs <nonfungibledevs@gmail.com>
2022-10-15 18:17:04 -0700

    actual v1 lol

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link href="/">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 font-Roboto text-3xl text-zinc-100">
+            <span className="ml-3 font-Roboto text-2xl text-zinc-100">
               Pakistan & Puerto Rico Charity Collective
             </span>
           </a>
